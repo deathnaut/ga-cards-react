135a3d5c8322fd8703f8a7883bfc6645ea26a67d
Karla Moreno <moreno_karla@live.com>
2017-10-27 12:04:04 -0500

    got live text update in "add" page working

--- ga-cards/src/components/Add.js
+++ ga-cards/src/components/Add.js
@@ -6,13 +6,28 @@ class Add extends Component {
     super(props);
 
     this.state = {
-
+      preview: "A preview appears here as you type..."
     }
+    this._updateText = this._updateText.bind(this);
+  };
+
+  _updateText(e){
+    this.setState({
+      preview: e.target.value
+    });
   }
+
   render() {
     return (
       <div className="App">
-        <h2>meow</h2>
+        <form name="add-card" id="add-card">
+          <input type="text" name="question" id="question" placeholder="What's your question?" value={this.state.preview} onChange={this._updateText}/>
+        </form>
+        <br/>
+        <div className="card">
+          <h4 className="card-title">{this.state.preview}</h4>
+          <h6>Cards Against Assembly</h6>
+        </div>
       </div>
     );
   }
